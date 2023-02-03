97a1d64e6defdd9b05d2048ac3f29c4c7718fc5b
Jordan Freeman <jordansolfreeman@outlook.com>
2023-02-03 00:04:09 -0800

    finished adding analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 import Layout from '../components/Layout'
+import {Analytics} from '@vercel/analytics/react'
 
 function MyApp({ Component, pageProps }: AppProps) {
 
@@ -18,9 +19,11 @@ function MyApp({ Component, pageProps }: AppProps) {
         <link rel="icon" type="image/png" href="/favicon.png"/>
         <link href="https://fonts.googleapis.com/css2?family=Barlow+Condensed:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"/>
       </Head>
+
       <Layout>
         <Component {...pageProps} />
       </Layout>
+      <Analytics/>
     </>
   ) 
 }
